549124cdc8c8c88fcd7a72682114790a94ed95c1
lucienfeel@gmail.com <lucienxu@tencent.com>
2015-08-14 19:19:08 +0800

    fix vidoe bug

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
     res.send('Hello WxMoment!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(30001, function () {
 
     var host = server.address().address;
     var port = server.address().port;
