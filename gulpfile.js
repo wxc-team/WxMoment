7519d75f1b8f66e6462b5533fb9191546a2403ef
lucienfeel@gmail.com <lucienxu@tencent.com>
2015-08-18 17:31:01 +0800

    fix analytics

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('build', function () {
 
     return gulp.src('./build/wxmoment.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/'));
+        .pipe(rename('wxmoment.min.js'))
+        .pipe(gulp.dest('./dist'));
 });
+
+gulp.task("default", ["build"]);
